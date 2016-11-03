4a947f3aebbd1e5114c261729ef66478337a7c2c
Adam Rice <rice.adam@gmail.com>
2016-11-03 00:35:27 -0400

    Moved hyperledger into guid dir

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -20,7 +20,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hughy603/hyperledger/fabric/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation
